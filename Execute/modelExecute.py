64bbbad787ddbdac77c5e38cd53e1db4bf0b700b
HamzaAmir <hamza2019amir@gmail.com>
2024-01-04 01:01:28 +0200

    small check

--- Execute/modelExecute.py
+++ Execute/modelExecute.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from Logs.modelLogger import modelLogger
 
+
 class modelExecute(ABC):
     def __init__(self, model_name, sharedConfig, tcnConfig):
         self.sharedConfig = sharedConfig
