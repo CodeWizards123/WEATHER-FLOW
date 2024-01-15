d02a06ea4e92321040688eb1bc56bd6fa78d5135
Adeeb Gaibie <gbxade002@myuct.ac.za>
2024-01-15 13:55:19 +0200

    tcn split

---
+++ GPUrunTCN_12h.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --account=a100free
+#SBATCH --partition=a100
+#SBATCH --nodes=1 --ntasks=4 --gres=gpu:a100-2g-10gb:1
+#SBATCH --time=25:00:00
+#SBATCH --job-name="tcn_gpu_12h"
+CUDA_VISIBLE_DEVICES=$(ncvd)
+nvcc --version
+module load python/miniconda3-py38-usr-A
+source activate weatherEnv
+python3 runTCN_12h.py
---
+++ runTCN_24h.py
@@ -0,0 +1,16 @@
+import yaml
+import Logs.Evaluation as Evaluation
+from Execute.tcnExecute import tcnExecute
+
+###########################################################
+
+with open('configurations/tcnConfig.yaml', 'r') as file:
+    tcnConfig = yaml.safe_load(file)
+with open('configurations/sharedConfig_TCN_24h.yaml', 'r') as file:
+    sharedConfig = yaml.safe_load(file)
+
+
+tcn_trainer = tcnExecute(sharedConfig, tcnConfig)
+tcn_trainer.execute()
+
+Evaluation.TcnEval(tcnConfig, sharedConfig)
