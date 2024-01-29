7b2334c8bac95e614670b77715464f5605790b4c
Adeeb Gaibie <gbxade002@myuct.ac.za>
2024-01-29 18:47:03 +0200

    Updated execution scripts and TCN model adjustments

--- GPUrunTCN_6h.sh
+++ GPUrunTCN_6h.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --account=a100free
 #SBATCH --partition=a100
-#SBATCH --nodes=1 --ntasks=4 --gres=gpu:a100-2g-10gb:1
+#SBATCH --nodes=1 --ntasks=8 --gres=gpu:a100-2g-10gb:1
 #SBATCH --time=25:00:00
 #SBATCH --job-name="tcn_gpu_6h"
 CUDA_VISIBLE_DEVICES=$(ncvd)
